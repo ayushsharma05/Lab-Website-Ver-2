e9b081b72589e5086b6908854f4cf4bf86db8e62
ayush <221320005@nitdelhi.ac.in>
2024-04-19 14:18:48 +0530

    Improved design

--- lab/Main/js/menu-navbar.js
+++ lab/Main/js/menu-navbar.js
@@ -2,12 +2,16 @@
 //-------------------------------------------------------------------------------------------------------------------------
 document.querySelector('.navbar-toggle').addEventListener('click', function(){
     var AllMenuElements = document.querySelectorAll('.navbar-nav > li');
+    var MainNavigation = document.querySelector('#main-navigation');
+    
     AllMenuElements.forEach(function(element){
         if(element.style.display === 'block'){
             element.style.display = 'none';
+            MainNavigation.style.display = 'none';
         }
         else{
             element.style.display = 'block';
+            MainNavigation.style.display = 'block'
         }
     });
 });
