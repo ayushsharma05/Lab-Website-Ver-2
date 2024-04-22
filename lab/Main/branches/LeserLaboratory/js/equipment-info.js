efec0ea3b8338c1b0a353f61a35e20b8a051eeb8
ayush <221320005@nitdelhi.ac.in>
2024-04-22 09:16:52 +0530

    modified:   lab/Main/branches/LeserLaboratory/js/equipment-info.js 	modified:   lab/Main/branches/LeserLaboratory/js/lab-info.js

--- lab/Main/branches/LeserLaboratory/js/equipment-info.js
+++ lab/Main/branches/LeserLaboratory/js/equipment-info.js
@@ -1,77 +1,49 @@
 // Arrays for equipment information
-///Upadtes info of equipment in this section
+///Updates info of equipment in this section
 //---------------------------------------------------------------------------------------------------------------
 console.log('equipment info requested');
 var imgSrcArray = [
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture1.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture2.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture3.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture4.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture5.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture6.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture7.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture8.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture9.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture10.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture11.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture12.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture13.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture14.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture15.jpg"
+    "../branches/LeserLaboratory/img/equipments/Picture1.png",
+    "../branches/LeserLaboratory/img/equipments/Picture2.png",
+    "../branches/LeserLaboratory/img/equipments/Picture3.jpg",
+    "../branches/LeserLaboratory/img/equipments/Picture4.jpg",
+    "../branches/LeserLaboratory/img/equipments/Picture5.jpg",
+    "../branches/LeserLaboratory/img/equipments/Picture6.jpg",
+    "../branches/LeserLaboratory/img/equipments/Picture7.jpg",
+    "../branches/LeserLaboratory/img/equipments/Picture8.jpg"
 ];
 
 var equipmentNameArray = [
-    "Liquid limit apparatus",
-    "Plastic limit apparatus",
-    "Balance (digital)",
-    "Oven",
-    "Hydrometer",
-    "Pycnometer",
-    "Motorized sieve shaker",
-    "Proving rings set",
-    "Metal tray",
-    "Poly carbonate beakers 25ml",
-    "Poly carbonate beakers 50ml",
-    "Polycarbonate beakers 100ml",
-    "Polycarbonate beakers 250ml",
-    "Polycarbonate beakers 500ml",
-    "Polycarbonate beakers 1000ml"
+    "UV - Vis Spectrophotometer",
+    "Kjeldahl Unit",
+    "Muffle Furnace",
+    "BOD Incubator",
+    "COD Digestor",
+    "Distillation Unit",
+    "pH Meter",
+    "Turbidity Meter"
 ];
 
 var equipmentInfoArray = [
-    "This apparatus determines the liquid limit of soil by measuring the moisture content at which soil transitions from a plastic to a liquid state.",
-    "Used to determine the plastic limit of soil by identifying the moisture content at which soil can no longer be molded without cracking.",
-    "A precision instrument for accurately measuring the mass of materials in grams or kilograms.",
-    "Used for drying soil samples and other materials at controlled temperatures to remove moisture content.",
-    " Measures the density of liquids or suspensions, commonly used in soil analysis to determine particle size distribution.",
-    "Measures the density or specific gravity of liquids, solids, or powders by displacement.",
-    "Automates the process of sieving soil or aggregates to obtain particle size distribution.",
-    "Measures the load or force exerted on soil or other materials during compaction or penetration tests.",
-    "Provides a flat, stable surface for conducting tests or holding samples in the laboratory.",
-    "Transparent containers of various sizes used for measuring, mixing, or storing liquids or solids in the laboratory.",
-    "Suitable for measuring and mixing moderate volumes of liquids or solids in laboratory settings.",
-    "Ideal for measuring, mixing, or storing moderate amounts of liquids or solids in laboratory experiments.",
-    "Suitable for measuring, mixing, or storing larger volumes of liquids or solids in laboratory procedures.",
-    "Commonly used for measuring, mixing, or storing substantial volumes of liquids or solids in laboratory experiments.",
-    "Suitable for measuring, mixing, or storing larger volumes of liquids or solids in laboratory procedures."
+    "UV-Vis spectrophotometer measures the absorption or reflectance of ultraviolet or visible light by a sample.",
+    "Kjeldahl unit is used for nitrogen determination by the Kjeldahl method, typically in organic chemistry.",
+    "Muffle furnace is a type of furnace used for high-temperature applications such as fusing glass, creating enamel coatings, and ceramics.",
+    "BOD incubator provides controlled conditions for biochemical oxygen demand (BOD) testing, typically used in water quality analysis.",
+    "COD digestor is used for chemical oxygen demand (COD) testing, determining the amount of organic compounds in water samples.",
+    "Distillation unit separates components of a liquid mixture based on differences in boiling points.",
+    "pH meter measures the acidity or alkalinity of a solution.",
+    "Turbidity meter measures the cloudiness or haziness of a liquid caused by suspended solids."
 ];
 
 var isCodeArray = [
-    "2720 (Part 5): 1985",
-    "2720 (Part 5): 1985",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987"
+    "Some IS code",
+    "Some IS code",
+    "Some IS code",
+    "Some IS code",
+    "Some IS code",
+    "Some IS code",
+    "Some IS code",
+    "Some IS code"
 ];
 
 //------------------------------------------------------------------------------------------------------------------------
