ae755e73a685c3095c3d12e4f21dfeb0a0273d04
ayush <221320005@nitdelhi.ac.in>
2024-04-28 12:32:46 +0530

    new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture1.jpeg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture1.png 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture10.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture11.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture12.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture13.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture14.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture15.jpg 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture2.jpeg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture2.png 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture3.jpg 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture3.png 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture4.jpg 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture4.png 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture5.jpeg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture5.jpg 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture6.jpeg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture6.jpg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture7.png 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture7.webp 	modified:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture8.png 	new file:   lab/Main/branches/ComputationalLaboratory/img/equipments/Picture9.jpeg 	deleted:    lab/Main/branches/ComputationalLaboratory/img/equipments/Picture9.png 	modified:   lab/Main/branches/ComputationalLaboratory/js/equipment-info.js 	modified:   lab/Main/js/equipment-generator.js

--- lab/Main/branches/ComputationalLaboratory/js/equipment-info.js
+++ lab/Main/branches/ComputationalLaboratory/js/equipment-info.js
@@ -1,77 +1,43 @@
-// Arrays for equipment information
-///Upadtes info of equipment in this section
+// Arrays for software information
 //---------------------------------------------------------------------------------------------------------------
-console.log('equipment info requested');
+console.log('software info requested');
 var imgSrcArray = [
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture1.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture2.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture3.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture4.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture5.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture6.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture7.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture8.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture9.png",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture10.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture11.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture12.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture13.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture14.jpg",
-    "../branches/GeotechnicalLaboratory/img/equipments/Picture15.jpg"
+    "../branches/ComputationalLaboratory/img/equipments/Picture1.jpeg",
+    "../branches/ComputationalLaboratory/img/equipments/Picture2.jpeg",
+    "../branches/ComputationalLaboratory/img/equipments/Picture3.png",
+    "../branches/ComputationalLaboratory/img/equipments/Picture4.png",
+    "../branches/ComputationalLaboratory/img/equipments/Picture5.jpeg",
+    "../branches/ComputationalLaboratory/img/equipments/Picture6.jpeg",
+    "../branches/ComputationalLaboratory/img/equipments/Picture7.webp",
+    "../branches/ComputationalLaboratory/img/equipments/Picture8.png",
+    "../branches/ComputationalLaboratory/img/equipments/Picture9.jpeg"
 ];
 
 var equipmentNameArray = [
-    "Liquid limit apparatus",
-    "Plastic limit apparatus",
-    "Balance (digital)",
-    "Oven",
-    "Hydrometer",
-    "Pycnometer",
-    "Motorized sieve shaker",
-    "Proving rings set",
-    "Metal tray",
-    "Poly carbonate beakers 25ml",
-    "Poly carbonate beakers 50ml",
-    "Polycarbonate beakers 100ml",
-    "Polycarbonate beakers 250ml",
-    "Polycarbonate beakers 500ml",
-    "Polycarbonate beakers 1000ml"
+    "FreeCAD",
+    "AutoCAD",
+    "Quantum GIS (QGIS)",
+    "SUMO - Simulation of Urban Mobility",
+    "GeoStudio Educational Version",
+    "Geo5",
+    "Plaxis Introductory version",
+    "MATLAB",
+    "ANSYS"
 ];
+
 
 var equipmentInfoArray = [
-    "This apparatus determines the liquid limit of soil by measuring the moisture content at which soil transitions from a plastic to a liquid state.",
-    "Used to determine the plastic limit of soil by identifying the moisture content at which soil can no longer be molded without cracking.",
-    "A precision instrument for accurately measuring the mass of materials in grams or kilograms.",
-    "Used for drying soil samples and other materials at controlled temperatures to remove moisture content.",
-    " Measures the density of liquids or suspensions, commonly used in soil analysis to determine particle size distribution.",
-    "Measures the density or specific gravity of liquids, solids, or powders by displacement.",
-    "Automates the process of sieving soil or aggregates to obtain particle size distribution.",
-    "Measures the load or force exerted on soil or other materials during compaction or penetration tests.",
-    "Provides a flat, stable surface for conducting tests or holding samples in the laboratory.",
-    "Transparent containers of various sizes used for measuring, mixing, or storing liquids or solids in the laboratory.",
-    "Suitable for measuring and mixing moderate volumes of liquids or solids in laboratory settings.",
-    "Ideal for measuring, mixing, or storing moderate amounts of liquids or solids in laboratory experiments.",
-    "Suitable for measuring, mixing, or storing larger volumes of liquids or solids in laboratory procedures.",
-    "Commonly used for measuring, mixing, or storing substantial volumes of liquids or solids in laboratory experiments.",
-    "Suitable for measuring, mixing, or storing larger volumes of liquids or solids in laboratory procedures."
+    "FreeCAD is a versatile software used for 3D modeling and design tasks, including architectural and mechanical projects.",
+    "AutoCAD is an industry-standard software known for its powerful drafting and documentation capabilities.",
+    "QGIS is widely used by GIS professionals and enthusiasts for its extensive features and community support.",
+    "SUMO is essential for transportation engineers and researchers working on traffic management and urban planning.",
+    "GeoStudio Student Edition offers a hands-on experience in geotechnical modeling for students and educators.",
+    "Geo5 is a comprehensive toolset for analyzing various geotechnical problems related to slope stability.",
+    "PLAXIS simplifies complex finite element modeling tasks, making it accessible to geotechnical engineers.",
+    "MATLAB's versatility makes it indispensable for civil engineers seeking to solve complex problems through computation.",
+    "ANSYS is widely used in engineering industries for its robust simulation capabilities and advanced analysis tools."
 ];
 
-var isCodeArray = [
-    "2720 (Part 5): 1985",
-    "2720 (Part 5): 1985",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987",
-    "3696: 1987"
-];
+var isCodeArray = [];
 
-//------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------
